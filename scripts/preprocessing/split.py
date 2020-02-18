95aed087c8df5f8ac1c005b5b9aaaa7b03952752
gabrielscabrera <gabriel.sigurd.cabrera@gmail.com>
2020-02-18 19:00:41 +0100

    Fix errors in secondary split functions

--- scripts/preprocessing/split.py
+++ scripts/preprocessing/split.py
@@ -20,14 +20,14 @@ def split_cols_idx(dataset, splits, test_size = 0.25):
     x_idx_high = x_idx[1:]
     y_idx_high = y_idx[1:]
 
-    y_idx = np.array([slice(i,j) for i,j in zip(y_idx_low, y_idx_high)])
     x_idx = np.array([slice(i,j) for i,j in zip(x_idx_low, x_idx_high)])
+    y_idx = np.array([slice(i,j) for i,j in zip(y_idx_low, y_idx_high)])
 
     x_idx, y_idx = np.meshgrid(x_idx, y_idx)
 
     x_idx = np.ravel(x_idx)
     y_idx = np.ravel(y_idx)
-    z_idx = np.array([slice(0, dims[2])]*len(x_idx))
+    z_idx = np.array([slice(None)]*len(x_idx))
 
     idx = [[i,j,k] for i,j,k in zip(x_idx, y_idx, z_idx)]
     idx = np.array(idx)
@@ -77,10 +77,10 @@ def split_cubes_idx(dataset, splits, test_size = 0.25):
     idx = [[i,j,k] for i,j,k in zip(x_idx, y_idx, z_idx)]
     idx = np.array(idx)
 
-    N_cols = idx.shape[0]
-    N_test = int(N_cols*test_size)
-    idx_test = np.random.choice(N_cols, N_test, replace = False)
-    idx_train = np.arange(0, N_cols)
+    N_cubes = idx.shape[0]
+    N_test = int(N_cubes*test_size)
+    idx_test = np.random.choice(N_cubes, N_test, replace = False)
+    idx_train = np.arange(0, N_cubes)
     idx_train = np.delete(idx_train, idx_test)
     np.random.shuffle(idx_train)
 
@@ -91,24 +91,25 @@ def split_slices_idx(dataset, splits, test_size = 0.25):
         splits – approximate number of slices to create (>= 25)
     """
     dims = dataset.dims
+    z_idx_max = (dims[2]//splits)*splits
 
-    z_idx = np.linspace(0, dims[2], splits, dtype = np.int64)
+    z_idx = np.linspace(0, z_idx_max, splits+1, dtype = np.int64)
 
     z_idx_low = z_idx[:-1]
     z_idx_high = z_idx[1:]
 
     z_idx = np.array([slice(i,j) for i,j in zip(z_idx_low, z_idx_high)])
 
-    x_idx = np.array([slice(0, dims[0])]*len(z_idx))
-    y_idx = np.array([slice(0, dims[1])]*len(z_idx))
+    x_idx = np.array([slice(None)]*len(z_idx))
+    y_idx = np.array([slice(None)]*len(z_idx))
 
     idx = [[i,j,k] for i,j,k in zip(x_idx, y_idx, z_idx)]
     idx = np.array(idx)
 
-    N_cols = idx.shape[0]
-    N_test = int(N_cols*test_size)
-    idx_test = np.random.choice(N_cols, N_test, replace = False)
-    idx_train = np.arange(0, N_cols)
+    N_slices = idx.shape[0]
+    N_test = int(N_slices*test_size)
+    idx_test = np.random.choice(N_slices, N_test, replace = False)
+    idx_train = np.arange(0, N_slices)
     idx_train = np.delete(idx_train, idx_test)
     np.random.shuffle(idx_train)
 
@@ -134,9 +135,6 @@ def test_train_split(dataset, fail_times, splits, mode, test_size = 0.25):
     perc = 0
     for m, sample in enumerate(dataset):
 
-        if m >= 3:
-            break
-
         if mode == 'col':
             indices_train, indices_test = \
             split_cols_idx(dataset, splits, test_size)
@@ -149,7 +147,7 @@ def test_train_split(dataset, fail_times, splits, mode, test_size = 0.25):
 
         for n, idx_train in enumerate(indices_train):
 
-            X_train.append(sample[idx_train[0], idx_train[1], idx_train[1]])
+            X_train.append(sample[idx_train[0], idx_train[1], idx_train[2]])
             y_train.append(fail_times[m])
 
             # LOADING BAR
@@ -160,7 +158,7 @@ def test_train_split(dataset, fail_times, splits, mode, test_size = 0.25):
 
         for n, idx_test in enumerate(indices_test):
 
-            X_test.append(sample[idx_test[0], idx_test[1]])
+            X_test.append(sample[idx_test[0], idx_test[1], idx_test[2]])
             y_test.append(fail_times[m])
 
             # LOADING BAR
@@ -172,18 +170,35 @@ def test_train_split(dataset, fail_times, splits, mode, test_size = 0.25):
     # LOADING BAR
     print(f'\rLoading Segments {100:3d}%')
 
-    X_train = np.array(X_train, dtype = np.uint8)
-    y_train = np.array(y_train, dtype = np.uint8)
-
-    X_test = np.array(X_test, dtype = np.uint8)
-    y_test = np.array(y_test, dtype = np.uint8)
-
-    return X_train, X_test, y_train, y_test
+    return np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test)
 
 if __name__ == "__main__":
     label = 'M8_1'
-    splits = 200
+    splits = 125
     dataset = config.bins[label]
     fail_times = config.bin_fail_times[label]
+
+    X_train, X_test, y_train, y_test = \
+    test_train_split(dataset, fail_times, splits, mode = 'col')
+
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+    del X_train
+    del X_test
+    del y_train
+    del y_test
+
+    X_train, X_test, y_train, y_test = \
+    test_train_split(dataset, fail_times, splits, mode = 'slice')
+
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+    del X_train
+    del X_test
+    del y_train
+    del y_test
+
     X_train, X_test, y_train, y_test = \
     test_train_split(dataset, fail_times, splits, mode = 'cube')
+
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
