f66474919348eead62fb4455e3859840a0408461
GabrielSCabrera <gabriel.sigurd.cabrera@gmail.com>
2020-09-25 18:57:10 +0200

    Added installer

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install python -y
-sudo apt-get install python3-pip -y
-sudo apt-get install git -y
-sudo apt-get install pipenv -y
+sudo apt-get update --assume-yes
+sudo apt-get upgrade --assume-yes
+sudo apt-get install --assume-yes python
+sudo apt-get install --assume-yes python3-pip
+sudo apt-get install --assume-yes git
+sudo apt-get install --assume-yes pipenv
 mkdir ~/Programs
 cd ~/Programs
 git pull https://github.com/GabrielSCabrera/MasterThesis/
