8a76e5703b0cd41f792469fdbd5d6ac16a187c2d
GabrielSCabrera <gabriel.sigurd.cabrera@gmail.com>
2020-09-25 19:25:20 +0200

    Added installer

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ sudo apt-get upgrade --assume-yes
 sudo apt-get install --assume-yes python
 sudo apt-get install --assume-yes python3-pip
 sudo apt-get install --assume-yes git
-sudo apt-get install --assume-yes pipenv
-mkdir ~/Programs
-cd ~/Programs
-git pull https://github.com/GabrielSCabrera/MasterThesis/
-cd ~/Programs/MasterThesis
+sudo python3.8 -m pip install pipenv
+cd $HOME
+mkdir -p $HOME/Programs
+cd $HOME/Programs
+git clone https://github.com/GabrielSCabrera/MasterThesis.git
+cd $HOME/Programs/MasterThesis
 make install
